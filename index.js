ad01e6902f9530dd009d5cbc3d52e4624de9865a
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-04-24 15:12:56 +0200

    JavaScript hry

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const btnElm = document.querySelectorAll('button');
+const gameCell = document.querySelectorAll('.cell-content');
 let player = 'circle';
 const playerIcon = document.querySelector('.game-header__player p img');
 
-for (let i = 0; i < btnElm.length; i += 1) {
-  btnElm[i].addEventListener('click', (ev) => {
+for (let i = 0; i < gameCell.length; i += 1) {
+  gameCell[i].addEventListener('click', (e) => {
     if (player === 'circle') {
-      ev.target.classList.add('board__field--circle');
+      e.target.classList.add('board__field--circle');
       playerIcon.src = 'icons/cross.svg';
       player = 'cross';
     } else {
-      ev.target.classList.add('board__field--cross');
+      e.target.classList.add('board__field--cross');
       playerIcon.src = 'icons/circle.svg';
       player = 'circle';
     }
-    btnElm[i].disabled = true;
+    gameCell[i].disabled = true;
   });
 }
